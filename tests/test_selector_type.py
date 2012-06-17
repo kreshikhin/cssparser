45384b905c6daef8df93692de319688c764d8cbf
tailrecursion <pelou85@gmail.com>
2012-06-17 16:05:05 +0400

    Add tests for universal and type selectors

---
+++ tests/test_selector_type.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.2
+# coding=utf-8
+
+import unittest
+
+import sys
+sys.path.append('../src/')
+
+import xml.dom.minidom
+from selector.type import Type
+
+class TestCss2Py(unittest.TestCase):
+    def setUp(self):
+        self.selector = Type('p')
+        self.dom = xml.dom.minidom.parseString('<div><p>hello</p></div>');
+    
+    def test_match(self):
+        divNode = self.dom.firstChild
+        self.assertFalse(self.selector.match(divNode))
+        pNode = divNode.firstChild
+        self.assertTrue(self.selector.match(divNode))
+        
+if __name__ == '__main__':
+    unittest.main()
