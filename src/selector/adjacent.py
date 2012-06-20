3c70867dd38e1b20b51f54e5057b327af887a0e9
DmitryAB <dmitriy.generalov@inbox.ru>
2012-06-20 14:45:23 +0400

    adjacent selector added

--- src/selector/adjacent.py
+++ src/selector/adjacent.py
@@ -1,6 +1,12 @@
 
 class Adjacent():
     """Matches any F element immediately preceded by a sibling element E."""
-    
+    def __init__(self,E,F):
+        self.e = E
+        self.f = F
     def match(self, node):
-        pass
+        if node.nextSibling is None:
+           return False
+        if node.nextSibling.nodeName == self.e:
+           return True
+        return False
