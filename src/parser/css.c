41466d7eb675be4f9e15f2a83cdc0864c8b30fd8
tailrecursion <pelou85@gmail.com>
2012-07-11 15:56:28 +0400

    Added python module

---
+++ src/parser/css.c
@@ -0,0 +1,71 @@
+#include <Python.h>
+
+static PyObject *SpamError;
+
+
+static PyObject *
+spam_system(PyObject *self, PyObject *args)
+{
+    const char *command;
+    int sts;
+
+    if (!PyArg_ParseTuple(args, "s", &command))
+        return NULL;
+    sts = system(command);
+    if (sts < 0) {
+        PyErr_SetString(SpamError, "System command failed");
+        return NULL;
+    }
+    return PyLong_FromLong(sts);
+}
+
+static PyMethodDef SpamMethods[] = {
+    {"system",  spam_system, METH_VARARGS,
+     "Execute a shell command."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+static struct PyModuleDef spammodule = {
+   PyModuleDef_HEAD_INIT,
+   "spam",   /* name of module */
+   NULL, /* spam_doc,  module documentation, may be NULL */
+   -1,       /* size of per-interpreter state of the module,
+                or -1 if the module keeps state in global variables. */
+   SpamMethods
+};
+
+PyMODINIT_FUNC
+PyInit_spam(void)
+{
+    PyObject *m;
+
+    m = PyModule_Create(&spammodule);
+    if (m == NULL)
+        return NULL;
+
+    SpamError = PyErr_NewException("spam.error", NULL, NULL);
+    Py_INCREF(SpamError);
+    PyModule_AddObject(m, "error", SpamError);
+    return m;
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    /* Add a built-in module, before Py_Initialize */
+    PyImport_AppendInittab("spam", PyInit_spam);
+
+    /* Pass argv[0] to the Python interpreter */
+    Py_SetProgramName((wchar_t*)argv[0]);
+
+    /* Initialize the Python interpreter.  Required. */
+    Py_Initialize();
+
+    /* Optionally import the module; alternatively,
+       import can be deferred until the embedded script
+       imports it. */
+    PyImport_ImportModule("spam");
+}
+
+
