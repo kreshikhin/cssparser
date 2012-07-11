8cbeefe69330bded5b13a1487db5fe9f138f7866
tailrecursion <pelou85@gmail.com>
2012-07-11 16:25:36 +0400

    Fixed extension import

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+from distutils.command.install_data import install_data
+
+class post_install(install_data):
+    def run(self):
+        # Call parent 
+        # install_data.run(self)
+        # Execute commands
+        print("Running")
+
+
+cssparser = Extension('css', sources = ['src/cssparser/css.c'])
 
 setup(
     name='css2py',
@@ -7,14 +18,14 @@ setup(
     url= 'https://github.com/pinocchio964/css2py',
     description= 'Experimental python css-based engine to styling custom xml files.',
     version='1.0',
-    #data_files=[('/usr/local/sbin', ['sire'])],
     packages=[
         'css2py',
+        'css2py.cssparser',
     ],
     package_dir={
-        'sire'              : 'src',
+        'css2py'              : 'src',
+        'css2py.cssparser'    : 'src/cssparser'
     },
-    #package_data={
-    #    'sire.web' : ['css/*.css', 'image/*.*', 'js/*.js', '*.html']
-    #}
+    ext_modules = [cssparser],
+    cmdclass={"install_data": post_install}
 )
